b80266b103fb7b1796c5bea7663f7244bacb67a3
Mohammed Kamil <kamlan19922@gmail.com>
2023-08-30 21:03:44 +0400

    added validation constraints to book record

--- src/main/java/com/polarbookshop/catalog/domain/Book.java
+++ src/main/java/com/polarbookshop/catalog/domain/Book.java
@@ -1,4 +1,15 @@
 package com.polarbookshop.catalog.domain;
 
-public record Book(String isbn, String title, String author, double price) {
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Positive;
+
+public record Book(
+        @NotEmpty(message = "books isbn is required")
+        String isbn,
+        @NotEmpty(message = "title is required")
+        String title,
+        @NotEmpty(message = "book author is required")
+        String author,
+        @Positive(message = "book price should be greater than zero")
+        double price) {
 }
