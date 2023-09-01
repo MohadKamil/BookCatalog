6f9e7ea69dc4e8e484fa828baf2db46cadd604c8
Mohammed Kamil <kamlan19922@gmail.com>
2023-09-01 09:33:21 +0400

    added conditional test loader class

---
+++ src/main/java/com/polarbookshop/catalog/demo/BooksTestData.java
@@ -0,0 +1,31 @@
+package com.polarbookshop.catalog.demo;
+
+import com.polarbookshop.catalog.domain.Book;
+import com.polarbookshop.catalog.domain.BooksRepository;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.annotation.Profile;
+import org.springframework.context.event.EventListener;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Profile("TestData")
+@Component
+public class BooksTestData {
+
+    private final BooksRepository repository;
+
+    public BooksTestData(BooksRepository repository) {
+        this.repository = repository;
+    }
+
+    @EventListener(ApplicationReadyEvent.class)
+    public void seedBooks() {
+        var books = List.of(
+                new Book("1","Thinking, Fast and Slow","Daniel Kahneman",44),
+                new Book("2","Learning Domain Driven Design","Vlad Khononov",100)
+        );
+
+        books.forEach(repository::saveBook);
+    }
+}
