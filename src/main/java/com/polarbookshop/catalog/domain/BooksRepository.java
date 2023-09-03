ccc94882ec5b9966f911e74f5041c7c0ca48afd5
Mohammed Kamil <kamlan19922@gmail.com>
2023-09-03 11:08:05 +0400

    updated delete operation with query attribute to implement the delete functionality

--- src/main/java/com/polarbookshop/catalog/domain/BooksRepository.java
+++ src/main/java/com/polarbookshop/catalog/domain/BooksRepository.java
@@ -1,6 +1,9 @@
 package com.polarbookshop.catalog.domain;
 
+import org.springframework.data.jdbc.repository.query.Modifying;
+import org.springframework.data.jdbc.repository.query.Query;
 import org.springframework.data.repository.ListCrudRepository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
 
@@ -8,5 +11,9 @@ public interface BooksRepository extends ListCrudRepository<Book,Long> {
 
     Optional<Book> findByIsbn(String isbn);
     boolean existsByIsbn(String isbn);
+
+    @Transactional
+    @Modifying
+    @Query("DELETE FROM book where isbn = :isbn")
     void deleteByIsbn(String isbn);
 }
