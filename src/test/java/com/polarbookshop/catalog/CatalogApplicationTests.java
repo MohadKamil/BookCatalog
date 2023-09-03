048a061d73929277f2d8e4d6f27a2baeefc8f295
Mohammed Kamil <kamlan19922@gmail.com>
2023-09-03 12:01:59 +0400

    enabled integration profile on spring ETE test to utilize test containers

--- src/test/java/com/polarbookshop/catalog/CatalogApplicationTests.java
+++ src/test/java/com/polarbookshop/catalog/CatalogApplicationTests.java
@@ -4,11 +4,13 @@ import com.polarbookshop.catalog.domain.Book;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.reactive.server.WebTestClient;
 
 import static org.assertj.core.api.Assertions.*;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@ActiveProfiles("integration")
 class CatalogApplicationTests {
 
     @Autowired
