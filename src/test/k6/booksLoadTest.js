c5fad707520b438cd025c488835054a74dcd25f6
Mohammed Kamil <kamlan19922@gmail.com>
2023-09-05 14:05:08 +0400

    load test settings

--- src/test/k6/booksLoadTest.js
+++ src/test/k6/booksLoadTest.js
@@ -2,6 +2,11 @@ import http from 'k6/http';
 import { sleep } from 'k6';
 
 const endpoint = "http://localhost:9001"
+
+export const options = {
+    vus: 3000,
+    duration: "100s"
+}
 export default function () {
     http.get(endpoint + "/books");
     sleep(1);
